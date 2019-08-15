6c311694045c62ffc2761c7c2c0ca0ddbf09d8bc
Tristan <tristan.salles@sydney.edu.au>
2019-08-15 15:48:19 +1000

    add second example

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,7 +1,8 @@
 #
 cd /live
 
-jupyter-notebook --port=8888 --ip='0.0.0.0' --no-browser --allow-root --NotebookApp.token=''
+jupyter-notebook --port=8888 --ip='0.0.0.0' --no-browser --allow-root \
+  --NotebookApp.token='' --NotebookApp.default_url="/tree/StartHere.ipynb"
 
 while true; do
   sleep 600
--- scripts/run-jupyter.sh
+++ scripts/run-jupyter.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 PASS=${NB_PASSWD:-""}
-OPEN=${START_NB:-""}
+OPEN=${START_NB:-"StartHere.ipynb"}
 PORT=${NB_PORT:-8888}
 #
-cd $NB_DIR
+cd /live
 
-jupyter-notebook --port=$NB_PORT --ip='0.0.0.0' --no-browser --allow-root \
-       --NotebookApp.token=$NB_PASSWD --NotebookApp.default_url="/tree/$START_NB"
+jupyter-notebook --port=8888 --ip='0.0.0.0' --no-browser --allow-root \
+  --NotebookApp.token='' --NotebookApp.default_url="/tree/StartHere.ipynb"
 
 # Don't exit
 
